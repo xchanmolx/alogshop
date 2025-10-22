b67aeb13c4102ef27d5d076d789bac73ec701169
Chiantine Manigos <cmanigos@gmail.com>
2025-10-22 23:07:18 +0800

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,17 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'Alog Shop';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error('There was an error!', error),
+      complete: () => console.log('Request completed')
+    });
+  }
 }

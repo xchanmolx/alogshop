ae8a2601e2e5e5cade8b0f11fa673268e2f46f58
Chiantine Manigos <cmanigos@gmail.com>
2025-11-02 15:58:48 +0800

    End of 120. Styling the cart

---
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, input } from '@angular/core';
+import { CartItem } from '../../../shared/models/cart';
+import { RouterLink } from "@angular/router";
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [
+    RouterLink,
+    MatButton,
+    MatIcon,
+    MatIconButton,
+    CurrencyPipe
+],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss',
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+}
--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../core/services/cart.service';
+import { CartItemComponent } from "./cart-item/cart-item.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
-
+  cartService = inject(CartService);
 }

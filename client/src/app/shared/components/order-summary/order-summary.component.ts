685d8278772caf70265e9724576fa1edabca9c16
Chiantine Manigos <cmanigos@gmail.com>
2025-11-04 20:01:35 +0800

    End of 121. Creating the order summary component

---
+++ client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-summary',
+  imports: [],
+  templateUrl: './order-summary.component.html',
+  styleUrl: './order-summary.component.scss',
+})
+export class OrderSummaryComponent {
+
+}
